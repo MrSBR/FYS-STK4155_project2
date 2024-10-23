3e1cf0abb213dc6dce828139f16dab534954dbd2
MrSBR <Jonas.baa@hotmail.com>
2024-10-23 09:03:04 +0200

    Using Seaborn to load Wisconsin data

--- src/load_data.py
+++ src/load_data.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 """ File creating dataset so we use the same throughout the project"""
 
@@ -11,8 +12,16 @@ def load_simple_data(N_samples: int = 100, noise: float = 0.0, seed: int = 2024)
     return X, y, x, a_true
 
 def load_wisconsin_data():
-    data = np.loadtxt("../data/wisconsin_data.csv", delimiter=",")
-    X = data[:, 1:]
-    y = data[:, 0]
+    # Read the data into a pandas DataFrame
+    data = pd.read_csv("../../data/Wisconsin.csv")
+
+    # Change 'B' to 1 and 'M' to 2 in column 2
+    data.loc[data['diagnosis'] == 'B', 'diagnosis'] = 1
+    data.loc[data['diagnosis'] == 'M', 'diagnosis'] = 2
+
+    x = data['area_mean']
+
+    X = np.column_stack((np.ones(len(x)), x, x**2))
+    y = data['diagnosis']
     return X, y
 
